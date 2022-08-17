a7377ebe0904dac34bda2551cfdae171532394b3
NastyaAverkina <anastasia.averkina@gmail.com>
2022-08-17 18:30:59 +0300

    add Example5 condition

---
+++ Example005_Conditionifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert user's name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria") 
+{
+    Console. WriteLine("Hooray,it's Maria!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
